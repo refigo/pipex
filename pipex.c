37a4faf0a875c8e951c03eabc84d6662b08e707c
Refi Go <64242999+refigo@users.noreply.github.com>
2021-12-04 11:14:10 +0900

    Add set command

--- pipex.c
+++ pipex.c
@@ -1,9 +1,26 @@
-#include <stdio.h>
+#include "pipex.h"
 
 int	pipex(char **argv)
 {
 	printf("Inputs are %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4]);
 	return (0);
+}
+
+void	exit_custom(int argc)
+{
+	printf("argc == %d\n", argc);
+	printf("The number of arguments is wrong!\n");
+	exit(1);
+}
+
+
+
+int	set_command(char **envp)
+{
+	int	i;
+
+	// get path
+	i = 0;
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -23,25 +40,22 @@ int	main(int argc, char **argv, char **envp)
 	}
 	printf("\n");
 	*/
-	if (argc == 5)
+	if (argc != 5)
 	{
-		printf("argc == %d\n", argc);
-		pipex(argv);
+		exit_custom(argc);
 	}
-	else
-	{
-		printf("argc == %d\n", argc);
-		printf("The number of inputs is wrong!\n");
-	}
+	set_command(envp);
+	printf("argc == %d\n", argc);
+	pipex(argv);
 	return (0);
 }
 
 /*
-Examples
+	Examples
 
-$> ./pipex infile ``ls -l'' ``wc -l'' outfile
-should be the same as “< infile ls -l | wc -l > outfile”
+	$> ./pipex infile ``ls -l'' ``wc -l'' outfile
+	should be the same as “< infile ls -l | wc -l > outfile”
 
-$> ./pipex infile ``grep a1'' ``wc -w'' outfile
-should be the same as “< infile grep a1 | wc -w > outfile”
+	$> ./pipex infile ``grep a1'' ``wc -w'' outfile
+	should be the same as “< infile grep a1 | wc -w > outfile”
 */
