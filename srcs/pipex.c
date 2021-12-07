a8baf08e7abfd342fee178dc67406855d64377f6
refigo <wlguq100@naver.com>
2021-12-07 14:50:47 +0900

    feature : confirm pipex execution

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -30,18 +30,69 @@ void	exit_free_data(t_pipex *data)
 ** =============================================================================
 */
 
+int	process_parent(t_pipex **data, int pid_child, int *pipe_a, int i)
+{
+	int	status;
+
+	if (i == 0)
+		close(pipe_a[1]);
+	else
+		close(pipe_a[0]);
+	waitpid(pid_child, &status, 0);
+	return (0);
+}
+
+int	process_child(t_pipex **data, char **envp, int *pipe_a, int i)
+{
+	int	fd;
+
+	if (i == 0)
+	{
+		close(pipe_a[0]);
+		fd = open((*data)->infile, O_RDONLY);
+		// todo: exception
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+		dup2(pipe_a[1], STDOUT_FILENO);
+		close(pipe_a[1]);
+	}
+	else
+	{
+		fd = open((*data)->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		// todo: exception
+		dup2(pipe_a[0], STDIN_FILENO);
+		close(pipe_a[0]);
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+	execve((*data)->to_exec[i], (*data)->command[i], envp);
+	exit(0);
+	// todo: free data
+	return (0);
+}
+
 int	pipex(t_pipex **data, char **envp)
 {
-	int	fd_in;
-	int	fd_out;
-	char	*buf;
+	int	pipe_a[2];
+	pid_t	pid_child;
+	int	i;
 
-	fd_in = open((*data)->infile, O_RDONLY);
-	read(fd_in, buf, BUFFER_SIZE);
-	close(fd_in);
-	fd_out = open((*data)->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	write(fd_out, buf, BUFFER_SIZE);
-	close(fd_out);
+	i = -1;
+	pipe(pipe_a);
+	// todo: exception
+	while (++i < 2)
+	{
+		pid_child = fork();
+		// todo: exception
+		if (!pid_child)
+		{
+			process_child(data, envp, pipe_a, i);
+		}
+		else
+		{
+			process_parent(data, pid_child, pipe_a, i);
+		}
+	}
 	return (0);
 }
 
@@ -59,7 +110,7 @@ int	main(int argc, char **argv, char **envp)
 	pipex(&data, envp);
 
 	// test
-	test_anything(envp);
+	//test_anything(envp);
 	// todo : free data or exit_free_data
 	return (0);
 }
